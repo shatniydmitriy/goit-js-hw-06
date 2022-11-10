09a2197f7f3784b7be4aec9d184c93340b892eb0
shatniydmitriy <pp16dima10@gmail.com>
2022-11-10 14:42:47 +0200

    Update task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,26 @@
 const inputElm = document.querySelector("#validation-input");
 inputElm.addEventListener("blur", onBlur);
+
 function onBlur(event) {
   const input = event.currentTarget;
   const inputLength = Number(input.dataset.length);
+
   if (input.value.length === inputLength) {
-    input.classList.remove("invalid");
-    input.classList.add("valid");
+    inputChenge();
   } else {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
+    elseInputChange();
   }
 }
+ 
+function inputChenge() {
+input.classList.remove("invalid");
+    input.classList.add("valid");
+}
+function elseInputChange() {
+  input.classList.remove("valid");
+    input.classList.add("invalid");
+}
+
+
+
+
